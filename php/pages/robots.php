cc36d74e7226bea44747edc0787fc4b6776e6508
alexonbstudio <alexonbstudio@gmail.com>
2020-05-30 01:06:52 +0200

    del

--- php/pages/robots.php
+++
@@ -1,9 +0,0 @@
-<?php header('Content-Type: text/html; charset=utf-8'); ?>
-User-agent: *
-Disallow: /assets/
-Disallow: /inc/
-Disallow: /blocker/
-Disallow: /langs/
-Disallow: /pages/
-
-Sitemap: <?php echo $website['info']['langs']['protocol'].'://'.$website['info']['site']['default-url']; ?>/sitemap.xml
