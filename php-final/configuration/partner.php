8e3b95cea8a8c86934f1e74aaf1edf46fbffef10
alexonbstudio <alexonbstudio@gmail.com>
2020-06-09 02:35:23 +0200

    Update partner.php

--- php-final/configuration/partner.php
+++ php-final/configuration/partner.php
@@ -1,11 +1,12 @@
 <?php
-
+# exemple $partner['planethoster']['sites']
 $partner = array(
 	'planethoster' => array(
 		'name' => 'PlanetHoster',
 		'url' => 'https://www.planethoster.com/goph-af81b6dedd8f43f',
 		'images' => 'https://alexonbstudio.fr/assets/',
-		'code' => 'PHA-alexonbstudio'
+		'code' => 'PHA-alexonbstudio',
+		'sites' => 'planethoster.com'
 	)
 );
 
