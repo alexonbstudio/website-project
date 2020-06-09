38f0a131bcb6d2faf25075fbe2bb74592dc5f15e
alexonbstudio <alexonbstudio@gmail.com>
2020-06-09 23:39:09 +0200

    Update badge.php

--- php-final/themes/seo/xml/badge.php
+++ php-final/themes/seo/xml/badge.php
@@ -1,25 +1,15 @@
 <?php header('Content-type: application/xml; charset=utf-8'); ?>
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <?php 
-echo '
-<browserconfig>
+echo '<browserconfig>
 	<msapplication>
 		<tile>
-			<square70x70logo src="'.$protocols.'://'.$sites['domain'].'/'.$images['dir'].'/pinned/mstile-70x70.png"/>
-			<square150x150logo src="'.$protocols.'://'.$sites['domain'].'/'.$images['dir'].'/pinned/mstile-150x150.png"/>
-			<wide310x150logo src="'.$protocols.'://'.$sites['domain'].'/'.$images['dir'].'/pinned/mstile-310x310.png"/>
-			<square310x310logo src="'.$protocols.'://'.$sites['domain'].'/'.$images['dir'].'/pinned/mstile-310x150.png"/>
-			<TileColor>#da532c</TileColor>
+            <square70x70logo src="'.$protocols.'://'.$sites['domain'].'/'.$images['manager']['pinned']['s:70x70'].'"/>
+            <square150x150logo src="'.$protocols.'://'.$sites['domain'].'/'.$images['manager']['pinned']['s:150x150'].'"/>
+            <square310x310logo src="'.$protocols.'://'.$sites['domain'].'/'.$images['manager']['pinned']['s:310x310'].'"/>
+            <wide310x150logo src="'.$protocols.'://'.$sites['domain'].'/'.$images['manager']['pinned']['s:310x150'].'"/>
+            <TileColor>'.$seo['browsers']['color']['title'].'</TileColor>
 		</tile>
-		<notification>
-			<polling-uri src="'.$protocols.'://'.$sites['domain'].'/rss.xml&amp;id=1"/>
-			<polling-uri2 src="'.$protocols.'://'.$sites['domain'].'/rss.xml&amp;id=2"/>
-			<polling-uri3 src="'.$protocols.'://'.$sites['domain'].'/rss.xml&amp;id=3"/>
-			<polling-uri4 src="'.$protocols.'://'.$sites['domain'].'/rss.xml&amp;id=4"/>
-			<polling-uri5 src="'.$protocols.'://'.$sites['domain'].'/rss.xml&amp;id=5"/>
-			<frequency>30</frequency>
-			<cycle>1</cycle>
-		</notification>
 	</msapplication>
 </browserconfig>
 ';
