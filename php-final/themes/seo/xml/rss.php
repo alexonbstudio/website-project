aced3327210e6cffddc950c2578b29422a9adb7a
alexonbstudio <alexonbstudio@gmail.com>
2020-06-09 10:19:32 +0200

    Update rss.php

--- php-final/themes/seo/xml/rss.php
+++ php-final/themes/seo/xml/rss.php
@@ -6,9 +6,29 @@
 		<description>'.$general['index']['description'].'</description>
 		<title>'.$sites['name'].'</title>
 		<item>
-			<link>'.$protocols.'://'.$sites['domain'].'/'.$general['index']['url'].'</link>
+			<link>'.$protocols.'://'.$sites['domain'].'/'.$general['index']['url']['default'].'</link>
 			<description>'.$general['index']['description'].'</description>
 			<title>'.$sites['name'].' '.$general['index']['title'].'</title>
+		</item>
+		<item>
+			<link>'.$protocols.'://'.$sites['domain'].'/'.$sponsor['index']['url']['default'].'</link>
+			<description>'.$sponsor['index']['description'].'</description>
+			<title>'.$sites['name'].' '.$sponsor['index']['title'].'</title>
+		</item>
+		<item>
+			<link>'.$protocols.'://'.$sites['domain'].'/'.$partner['index']['url']['default'].'</link>
+			<description>'.$partner['index']['description'].'</description>
+			<title>'.$sites['name'].' '.$partner['index']['title'].'</title>
+		</item>
+		<item>
+			<link>'.$protocols.'://'.$sites['domain'].'/'.$email['index']['url']['default'].'</link>
+			<description>'.$partner['index']['description'].'</description>
+			<title>'.$sites['name'].' '.$email['index']['title'].'</title>
+		</item>
+		<item>
+			<link>'.$protocols.'://'.$sites['domain'].'/'.$law['pages']['url']['default'].'</link>
+			<description>'.$law['pages']['description'].'</description>
+			<title>'.$sites['name'].' '.$law['pages']['title'].'</title>
 		</item>
 	</channel>
 </rss>'; ?>
