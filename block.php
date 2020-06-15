35dff93e16bed3c1b5ab182ad868c6ade6917b4a
alexonbstudio <alexonbstudio@gmail.com>
2020-06-15 19:55:15 +0200

    Update block.php

--- block.php
+++ block.php
@@ -16,8 +16,8 @@ include_once 'configuration/hosting.php';
 include_once 'configuration/credits.php';
 include_once 'configuration/marketing.php';
 include_once 'configuration/images.php';
-/*
 include_once 'configuration/business.php';
+/*
 include_once 'configuration/market.php';
 include_once 'configuration/restaurant.php';
 */
@@ -34,9 +34,9 @@ $social = json_decode($JE_social, true);
 $hosting = json_decode($JE_hosting, true);
 $marketing = json_decode($JE_marketing, true);
 $images = json_decode($JE_images, true);
+$business = json_decode($JE_business, true);
 /*
 #Supplémentaire
-$business = json_decode($JE_business, true);
 $markets = json_decode($JE_markets, true);
 $restaurant = json_decode($JE_restaurant, true);
 */
