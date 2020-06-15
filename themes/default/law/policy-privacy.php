3468961b18b56eb8b05b09d8c5e19c76db7ce853
alexonbstudio <alexonbstudio@gmail.com>
2020-06-15 20:00:20 +0200

    Update policy-privacy.php

--- themes/default/law/policy-privacy.php
+++ themes/default/law/policy-privacy.php
@@ -3,16 +3,16 @@
   <div class="container">
 
     <!-- Page Heading/Breadcrumbs -->
-    <h1 class="mt-4 mb-3"><?php echo $law['pages']['name']; ?>
+    <h1 class="mt-4 mb-3"><?php echo $law['pages']['title']; ?>
       <small><?php echo $title; ?></small>
     </h1>
 
     <ol class="breadcrumb">
       <li class="breadcrumb-item">
-        <a href="<?php echo $protocols.'://'.$sites['domain'].'/'.$general['index']['url']; ?>"><?php echo $general['index']['name']; ?></a>
+        <a href="<?php echo $protocols.'://'.$sites['domain'].'/'.$general['index']['url']; ?>"><?php echo $general['index']['title']; ?></a>
       </li>
       <li class="breadcrumb-item">
-        <a href="<?php echo $protocols.'://'.$sites['domain'].'/'.$law['pages']['url']; ?>"><?php echo $law['pages']['name']; ?></a>
+        <a href="<?php echo $protocols.'://'.$sites['domain'].'/'.$law['pages']['url']['default']; ?>"><?php echo $law['pages']['title']; ?></a>
       </li>
       <li class="breadcrumb-item active"><?php echo $title; ?></li>
     </ol>
