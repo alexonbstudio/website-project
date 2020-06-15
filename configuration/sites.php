a8705b1d92e3e68c0c1e7c69c3afa9891ff4d399
alexonbstudio <alexonbstudio@gmail.com>
2020-06-15 19:56:30 +0200

    Update sites.php

--- configuration/sites.php
+++ configuration/sites.php
@@ -1,6 +1,6 @@
 <?php
 /*
-exemple $sites['head']['robots'];
+exemple $sites['debug'];
 exemple $sites['update']['rdf'];
 exemple $sites['e-mail']['contact'];
 
@@ -8,7 +8,7 @@ exemple $sites['e-mail']['contact'];
 $sites = array(
 	'name' => 'Website Project (WP)',
 	/*'domain' => 'website-project.test',=Nginx test*/ /*domain: exemple.tld*/
-	'domain' => 'localhost', /*domain: exemple.tld*/
+	'domain' => 'dev.alexonbstudio.fr', /*domain: exemple.tld*/
 	'protocol' => isset($_SERVER["HTTPS"]) ? 'https' : 'http',
 	'template' => 'default',
 	'create' => array(),
@@ -16,11 +16,11 @@ $sites = array(
 		'rdf' => '2020-06-07'
 	),
 	'copyright' => array(
-		'frontend' => 'Copyright &copy; 2019-'.date('Y'),
-		'rdf' => 'Copyright &copy;'
+		'frontend' => 'Copyright &copy; 2019-'.date('Y'), /*show on template */
+		'rdf' => 'Copyright &copy;' /*show only template seo/txt/rdf*/
 	),
 	'head' => array(
-		'robots' => 'noopd, noydir'
+		'robots' => 'noopd, noydir' /*Only show on template header.php | robots meta*/
 	),
 	'default-timezone' => 'Etc/UTC' /*Docs PHP variable date_default_timezone_set() */
 );
