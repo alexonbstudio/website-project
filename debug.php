1f9d3ea069f921a62847cf7560764f4d5ae82b82
alexonbstudio <alexonbstudio@gmail.com>
2020-07-16 11:10:32 +0200

    Add debug (for dev JSON datz parse)
    its simple

--- debug.php
+++ debug.php
@@ -1,6 +1,8 @@
 <?php
+
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
+
 
 require 'libs/custom/compress.php'; 
 #configuration
@@ -22,76 +24,132 @@ include_once 'configuration/market.php';
 include_once 'configuration/restaurant.php';
 */
 
-    include_once 'languages/'.$translate['auto']['files'].'/general.php'; 
-    include_once 'languages/'.$translate['auto']['files'].'/partner.php'; 
-    include_once 'languages/'.$translate['auto']['files'].'/sponsor.php'; 
-    include_once 'languages/'.$translate['auto']['files'].'/law.php'; 
-    include_once 'languages/'.$translate['auto']['files'].'/email.php'; 
-    include_once 'languages/'.$translate['auto']['files'].'/block.php'; 
-    include_once 'languages/'.$translate['auto']['files'].'/sitemap.php'; 
+
+#Decode
+$sites = json_decode($JE_sites, true);
+$sponsor_config = json_decode($JE_sponsor_config, true);
+$partner_config = json_decode($JE_partner_config, true);
+$translate = json_decode($JE_translate, true);
+$credits = json_decode($JE_credits, true);
+$private = json_decode($JE_private, true);
+$seo = json_decode($JE_seo, true);
+$social = json_decode($JE_social, true);
+$hosting = json_decode($JE_hosting, true);
+$marketing = json_decode($JE_marketing, true);
+$images = json_decode($JE_images, true);
+$business = json_decode($JE_business, true);
 
 
+#Syslink
+$protocols = $sites['protocol'];
+$Languages_translate = isset($_SERVER["HTTP_ACCEPT_LANGUAGE"]) ? $translate['auto']['seo'] : $translate['manual']['frontend']['french'];
+
+#Configuration
+$lang_finales = 'languages/'.$Languages_translate.'/general.php';
+if (file_exists($lang_finales)) {
+    include_once 'languages/'.$Languages_translate.'/general.php'; 
+    include_once 'languages/'.$Languages_translate.'/partner.php'; 
+    include_once 'languages/'.$Languages_translate.'/sponsor.php'; 
+    include_once 'languages/'.$Languages_translate.'/law.php'; 
+    include_once 'languages/'.$Languages_translate.'/email.php'; 
+    include_once 'languages/'.$Languages_translate.'/block.php'; 
+    include_once 'languages/'.$Languages_translate.'/sitemap.php'; 
+    include_once 'languages/'.$Languages_translate.'/about.php'; 
+    include_once 'languages/'.$Languages_translate.'/pool-services.php'; 
+    include_once 'languages/'.$Languages_translate.'/pricing.php'; 
+    include_once 'languages/'.$Languages_translate.'/faqs.php'; 
+    include_once 'languages/'.$Languages_translate.'/others.php'; 
+    include_once 'languages/'.$Languages_translate.'/debug.php'; 
+} else {
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/general.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/partner.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/sponsor.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/law.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/email.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/block.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/sitemap.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/about.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/pool-services.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/pricing.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/faqs.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/others.php'; 
+    include_once 'languages/'.$translate['manual']['frontend']['french'].'/debug.php'; 
+}
+
+#Translate
+$general = json_decode($JE_translate_general, true);
+$partner = json_decode($JE_translate_partner, true);
+$sponsor = json_decode($JE_translate_sponsor, true);
+$law = json_decode($JE_translate_law, true);
+$email = json_decode($JE_translate_email, true);
+$block = json_decode($JE_translate_block, true);
+$sitemap = json_decode($JE_translate_sitemap, true);
+$about = json_decode($JE_translate_about, true);
+$pool = json_decode($JE_translate_pool, true);
+$pricing = json_decode($JE_translate_pricing, true);
+$faqs = json_decode($JE_translate_faqs, true);
+$others = json_decode($JE_translate_others, true);
+$debug = json_decode($JE_translate_debug, true);
+
+#Email contact form PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+#use PHPMailer\PHPMailer\Exception; test
+require 'libs/phpmailer/src/PHPMailer.php';
+$mail = new PHPMailer(true);
+			
+#frontend
+if(isset($_GET['lang'])){
+	if($_GET['lang'] == $Languages_translate){
+		if(isset($_GET['pages'])){
+			if($_GET['pages'] == 'index'){
+				if(isset($_GET['json'])){
+					if($_GET['json'] == 'sites'){
+						include_once('themes/debug/sites.php');
+					} else if($_GET['json'] == 'social'){
+						include_once('themes/debug/social.php');
+					} else if($_GET['json'] == 'private'){
+						include_once('themes/debug/private.php');
+					} else if($_GET['json'] == 'business'){
+						include_once('themes/debug/business.php');
+					} else if($_GET['json'] == 'images'){
+						include_once('themes/debug/images.php');
+					} else if($_GET['json'] == 'partner'){
+						include_once('themes/debug/partner.php');
+					} else if($_GET['json'] == 'sponsor'){
+						include_once('themes/debug/sponsor.php');
+					} else if($_GET['json'] == 'credits'){
+						include_once('themes/debug/credits.php');
+					} else {
+						header('Location: '.$protocols.'://'.$sites['domain']);
+						exit();
+					}
+				} else {
+					$title = $debug['index']['title'];
+					$description = $debug['index']['description'];
+					$keyword = $debug['index']['keyword'];
+					$urls = $debug['index']['url']['default'];
+					$imgs = $debug['index']['sitemap']['images'];
+					define('__WP_FR_URL__', $translate['manual']['frontend']['french'].'/'.$debug['index']['url']['fr']);
+					define('__WP_EN_URL__', $translate['manual']['frontend']['english'].'/'.$debug['index']['url']['en']);
+					include('themes/'.$sites['template'].'/header.php');
+					include_once('themes/'.$sites['template'].'/debug/full.php');
+					include('themes/'.$sites['template'].'/footer.php');
+				}
+			} else {
+				header('Location: '.$protocols.'://'.$sites['domain']);
+				exit();
+			}
+		} else {
+			header('Location: '.$protocols.'://'.$sites['domain']);
+			exit();
+		}
+	} else {
+		header('Location: '.$protocols.'://'.$sites['domain']);
+		exit();
+	}
+} else {
+	header('Location: '.$protocols.'://'.$sites['domain'].'/'.$debug['index']['url']['default']);
+	exit();
+}
 
 ?>
-<html>
-	<head>
-		<title>
-			Website Project (WP) - Debug json V1.5
-		</title>
-	</head>
-	<body>
-		<h2>Sites - Json</h2>
-		<?php echo $JE_sites; ?>
-		
-		<h2>translate - Json</h2>
-		<?php echo $JE_translate; ?>
-		
-		<h2>credits - Json</h2>
-		<?php echo $JE_credits; ?>
-		
-		<h2>private - Json</h2>
-		<?php echo $JE_private; ?>
-		
-		<h2>seo - Json</h2>
-		<?php echo $JE_seo; ?>
-		
-		<h2>partner - Json</h2>
-		<?php echo $JE_partner; ?>
-		
-		<h2>social - Json</h2>
-		<?php echo $JE_social; ?>
-		
-		<h2>hosting - Json</h2>
-		<?php echo $JE_hosting; ?>
-		
-		<h2>marketing - Json</h2>
-		<?php echo $JE_marketing; ?>
-		
-		<h2>images - Json</h2>
-		<?php echo $JE_images; ?>
-		<p style="text-align: center;">################Translate LANGS ISO  639-1 ##################</p>
-		<h2>general - Json</h2>
-		<?php echo $JE_translate_general; ?>
-		
-		<h2>partner - Json</h2>
-		<?php echo $JE_translate_partner; ?>
-		
-		<h2>sponsor - Json</h2>
-		<?php echo $JE_translate_sponsor; ?>
-		
-		<h2>law - Json</h2>
-		<?php echo $JE_translate_law; ?>
-		
-		<h2>email - Json</h2>
-		<?php echo $JE_translate_email; ?>
-		
-		<h2> block - Json</h2>
-		<?php echo $JE_translate_block; ?>
-		
-		<h2>sitemap - Json</h2>
-		<?php echo $JE_translate_sitemap; ?>
-		<h4 style="text-align: center;">##########		Copyright &copy; AlexonbStudio CC-BY-SA - GNU GPL V3.0		##########</h4>
-		
-	</body>
-<html>
-
