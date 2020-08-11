303cff91da886872c90fceb445963f8d056b8cc5
alexonbstudio <alexonbstudio@gmail.com>
2020-08-11 16:20:27 +0200

    missing betweeb head
    Add Google Tags Manager code

--- themes/knight/header.php
+++ themes/knight/header.php
@@ -470,6 +470,16 @@ if(!empty($marketing['google']['adsense'])){
 }
 
 
+if(!empty($seo['google']['tag-manager'])){
+	echo '<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
+	new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
+	\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,\'script\',\'dataLayer\',\''.$seo['google']['tag-manager'].'\');</script>
+	<!-- End Google Tag Manager -->';
+}
+
 if(!empty($seo['shareaholic']['key'])){
 	 echo '<!-- BEGIN SHAREAHOLIC CODE -->
 <link rel="preload" href="https://cdn.shareaholic.net/assets/pub/shareaholic.js" as="script" />
